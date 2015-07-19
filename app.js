848f639b55baa777ae173f4b6a07db888f0d6fa8
Your Name <you@example.com>
2015-07-19 22:06:02 +0000

    i made this

--- app.js
+++ app.js
@@ -5,10 +5,31 @@ appHelper.runApp(function(app, db) {
 
 	console.log("Travel application is running!");
 
-	var myName = "Laura";
+	var myName = "Laura Isabel";
 	app.get("/", function (req, resp) {
 		resp.write("<html><body><h1>Welcome to "+myName+"'s first web app!</body></html>");
 		resp.end();
 	});
+  
+  app.get("/icecream/:flavor?", function(req, resp){
+    resp.render("icecream", {flavor: req.params.flavor});
+  })
+  
+  app.get("/travel", function(req, resp) {
+   db.findArray({}, function(results) {
+     resp.render("travel", {
+       locations: results
+     })
+   })
+  })
+  
+  app.get("/travel/rating/:rating", function(req, resp) {
+    var rating = parseInt(req.params.rating);
+    db.findArray({rating: rating}, function(results) {
+     resp.render("travel", {
+       locations: results
+     })
+   })
+  })
 
 })
